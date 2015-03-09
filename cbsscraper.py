a8b0f011b44065b60affbc9f23af79e25a615318
Joshua Plante <jplan002@cslab-127.cs.plattsburgh.edu>
2015-03-09 10:38:52 -0400

    Cbs rss scraper

--- cbsscraper.py
+++ cbsscraper.py
@@ -1,16 +1,28 @@
-#! usr/bin/env Python3
+#! usr/bin/env python3
 
 import urllib.request
+
+def stopRead(title):
+    i = 0
+    read=[]
+    while title[i] != '<':
+        read.append(title[i])
+        i+=1
+        
+    result = ''.join(read)
+    return result
+    
+#def links():
 
 def main():
     cbs = urllib.request.urlopen('http://www.cbsnews.com/latest/rss/main')
     info = str(cbs.read())
+    
+    
+    pos = info.find('/news/')
+    story1 = info[pos+6:pos+100]
+    
 
-    for i in info:
-        while i == '<link>':
-            print(i)
-            if i == '<\link>':
-                break
-            break
+    print("the top story is", stopRead(story1))
 
 main()
