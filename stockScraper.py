933d3266ea504d5e75d65ac6ee19f717db85d94f
William Burke <wburk001@cslab-156.cs.plattsburgh.edu>
2015-05-10 18:00:27 -0400

    Added graph for the data

--- stockScraper.py
+++ stockScraper.py
@@ -4,8 +4,11 @@ import urllib.request
 import threading
 from time import gmtime, strftime
 import sqlite3
+from datetime import datetime
 
 DATABASE = 'stocks.db'
+con = sqlite3.connect(DATABASE) #Open DB
+cur = con.cursor()
 
 def getPrice():
     url = urllib.request.urlopen('http://finance.yahoo.com/q?s=aapl')
@@ -14,26 +17,32 @@ def getPrice():
     priceCords = js[price+18:price+24] #Gets just the price
     return priceCords
 
-def startLoop():
-    t = threading.Timer(5, main).start() #Runs main() every 5 seconds
+def loop():
+    t = threading.Timer(60, main).start() #Runs main() every 5 seconds
+
+def makeTable(name):
+    cur.execute("""CREATE TABLE ? (price, time, date)""", name)
 
 def dataBase(price, time):
     date = strftime("%b %d, %Y", gmtime())
-    con = sqlite3.connect(DATABASE) #Open DB
-    cur = con.cursor()
-    #cur.execute("""CREATE TABLE apple (price, time, date)""")
     #Adds the price and time to the database
-    cur.execute("insert into apple values (?,?,?)",(price,time,date))
+    
+    cur.execute("insert into apple values (?,?,?)",(price, time, date))
     con.commit()
     for row in cur.execute('SELECT * FROM apple ORDER BY date, time'):
-        print(row) #Prints each entry in the database
-    con.close()
+        #print(row) #Prints each entry in the database
+        last = row
+    return row
+
+def printAll():
+    for row in cur.execute('SELECT * FROM apple ORDER BY price, time'):
+        print(row)
 
 def main():
-    time = strftime("%H:%M:%S", gmtime()) #Gets time
+    time = datetime.now().strftime("%H:%M.%S") #Gets time 9:30.30
+##    if("09:30.00" < time < "16:00.00"): #Stock market closes at 4pm
+##        loop()
     price = getPrice()
-    dataBase(price, time) #Writes to the database
-    
-    #print("Apple's current stock price is", price, "at", time)
-    #startLoop()
+    printAll()
+    #print(dataBase(price, time)) #Writes to the database
 main()
